8fb58ff1d57f64300dad9c769582fc58924d4a7d
Sean Hart <sean@tangojlabs.com>
2023-05-31 23:22:47 -0500

    turning on eatkiwi

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from farcaster import Warpcast
 from eatkiwi.listeners.farcaster import stream_casts, stream_notifications
 
 
-mnemonic = config("FARCASTER_MNEMONIC_DEV01")
-fname = config("FARCASTER_FNAME_DEV01")
+mnemonic = config("FARCASTER_MNEMONIC_EATKIWI")
+fname = config("FARCASTER_FNAME_EATKIWI")
 
 # The client will automagically cycle through access tokens
 client = Warpcast(mnemonic, rotation_duration=1)
