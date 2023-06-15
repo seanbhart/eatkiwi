deb7c699bb5b8dc87be5bf777220e395cc694872
Sean Hart <sean@tangojlabs.com>
2023-06-15 12:52:33 -0500

    added catch for ChunkedEncodingError in stream_casts

--- eatkiwi/farcaster/listeners.py
+++ eatkiwi/farcaster/listeners.py
@@ -53,41 +53,54 @@ def stream_casts(commands_instance) -> None:
     db = mongo_client[config("MONGO_DB_FC")]
     collection = db[config("MONGO_FC_COLLECTION_LINKS")]
 
-    # Stream new casts, and if a link is found, repost to FC
-    for cast in fcc.stream_casts():
-        if not cast: continue
-            
-        current_cast = cast
-        link = extract_link(current_cast.text)
-        if link is None: continue
-        
-        # these domains should be avoided
-        domains = config("BANNED_DOMAINS").split(',')
-        if check_url_contains_domains(link, domains): continue
-        
-        # don't post the same link twice
-        result = collection.find_one({"link": link})
-        if result: continue
-        
+    max_retries = 3
+    retry_delay = 5
+
+    while True:
         try:
-            # check if the link contains web3 content
-            page_content = get_text_from_webpage(link)
-            if not page_content: continue
-            answer, title = check_link_for_web3_content(page_content)
-            if not answer:
-                # store rejected links in the database as well
-                collection.insert_one({"link": link})
-                continue
-        except Exception as e:
-            logging.error(f"Failed getting title or saving link: {e}")
-            continue
-        
-        try:
-            # Post links directly in the text, not as an attachment or embed
-            # to ensure the link is visible in the cast received by the notification stream (if someone mentions the content)
-            fcc.post_cast(f"🥝 {title}\n{link}")
-            # fcc.post_cast(f"🥝 reply \"in the style of ___\" for a title and summary of this link 🥝\n\n{title}\n{link}")
-            collection.insert_one({"link": link})
-        except Exception as e:
-            logging.error(f"[stream_casts] Failed sending message: {e}")
-            continue
+            # Stream new casts, and if a link is found, repost to FC
+            for cast in fcc.stream_casts():
+                if not cast: continue
+                    
+                current_cast = cast
+                link = extract_link(current_cast.text)
+                if link is None: continue
+                
+                # these domains should be avoided
+                domains = config("BANNED_DOMAINS").split(',')
+                if check_url_contains_domains(link, domains): continue
+                
+                # don't post the same link twice
+                result = collection.find_one({"link": link})
+                if result: continue
+                
+                try:
+                    # check if the link contains web3 content
+                    page_content = get_text_from_webpage(link)
+                    if not page_content: continue
+                    answer, title = check_link_for_web3_content(page_content)
+                    if not answer:
+                        # store rejected links in the database as well
+                        collection.insert_one({"link": link})
+                        continue
+                except Exception as e:
+                    logging.error(f"Failed getting title or saving link: {e}")
+                    continue
+                
+                try:
+                    # Post links directly in the text, not as an attachment or embed
+                    # to ensure the link is visible in the cast received by the notification stream (if someone mentions the content)
+                    fcc.post_cast(f"🥝 {title}\n{link}")
+                    # fcc.post_cast(f"🥝 reply \"in the style of ___\" for a title and summary of this link 🥝\n\n{title}\n{link}")
+                    collection.insert_one({"link": link})
+                except Exception as e:
+                    logging.error(f"[stream_casts] Failed sending message: {e}")
+                    continue
+
+        except ChunkedEncodingError as e:
+                max_retries -= 1
+                if max_retries <= 0:
+                    logging.error(f"Failed to stream casts after multiple retries: {e}")
+                    break
+                logging.warning(f"ChunkedEncodingError occurred, retrying in {retry_delay} seconds: {e}")
+                time.sleep(retry_delay)
